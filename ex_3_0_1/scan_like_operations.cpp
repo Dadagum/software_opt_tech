ab8c10b47367969dbb9f4b95a085f5af6a6957a6
Dadagum <471944704@qq.com>
2020-12-08 22:03:37 +0800

    finish all matches

--- ex_3_0_1/scan_like_operations.cpp
+++ ex_3_0_1/scan_like_operations.cpp
@@ -10,46 +10,29 @@
 #define NOT_EXSIST -2147483648
 using namespace std;
 
-static int mask_buffer[AVX_S+10];
-
-// 除了 avx-512 支持，貌似没有函数能够直接转化为 mask，只能暂时手动在内存中转化，找到再改
-int SIMD_bit_vector(__m256i x) {
-   // _mm256_storeu_si256((__m256i*)mask_buffer, x);
-    int mask = 0;
-    for (int i = 0; i < AVX_S; ++i) {
-        //int flag = mask_buffer[i] != 0;
-       // mask += flag << i;
-	}
-    return mask;
-}
+static int mask_buffer[AVX_S];
 
 int find_first_match_simd(int *x, int *y, int size, int left, int right) {
     // 首先进行对齐(暂时不做)
-    cout << "in first match" << endl;
+
+    
     __m256i LEFT = _mm256_set1_epi32(left);
     __m256i RIGHT = _mm256_set1_epi32(right);
     int i = 0;
     for (; i + AVX_S <= size; i += AVX_S) {
-        cout << "processing i " << i  << endl;
         __m256i X = _mm256_loadu_si256((__m256i*)(x+i));
-        __m256i f1 = _mm256_cmpgt_epi32(RIGHT, X);
-        __m256i f2 = _mm256_cmpgt_epi32(X, LEFT);
-        __m256i con = _mm256_and_si256(f1, f2);
-        cout << "pass con " << endl;
-        //int mask = SIMD_bit_vector(con);
+        // SIMD_condition
+        __m256i con = _mm256_and_si256(_mm256_cmpgt_epi32(RIGHT, X), _mm256_cmpgt_epi32(X, LEFT));
+        
+        // 除了 AVX-512 貌似没有办法转换为掩码，因此只好暂时出此下册，在内存中手动算
         _mm256_storeu_si256((__m256i*)mask_buffer, con);
         int mask = 0;
-            for (int i = 0; i < AVX_S; ++i) {
-                cout << "mask_buffer[i] = " << mask_buffer[i] << endl;
+        for (int i = 0; i < AVX_S; ++i) {
                 int flag = mask_buffer[i] != 0;
                 mask += flag << i;
         }
-
-        //int mask = 0;
-        cout << "mask = " << mask << endl;
         if (mask != 0) {
             for (int j = 0; j < AVX_S; ++j) {
-
                 if (mask >> j & 1){
                     return *(y+i+j);
                 }
@@ -57,7 +40,8 @@ int find_first_match_simd(int *x, int *y, int size, int left, int right) {
             
         }
     }
-    cout << "oh!" << endl;
+
+    // 说明前面都没有 match，那么对剩下的部分计算
     for (; i < size; ++i) {
         if (left <= x[i] && x[i] <= right){
             return y[i];
@@ -67,7 +51,42 @@ int find_first_match_simd(int *x, int *y, int size, int left, int right) {
 }
 
 // 查看 x 数组中所有范围在 [left, right] 的 y 值列表
-std::vector<int> find_all_matches_simd(int *x, int *y, int size, int left, int right);
+std::vector<int> find_all_matches_simd(int *x, int *y, int size, int left, int right) {
+    std::vector<int> res;
+
+    __m256i LEFT = _mm256_set1_epi32(left);
+    __m256i RIGHT = _mm256_set1_epi32(right);
+    int i = 0;
+    for (; i + AVX_S <= size; i += AVX_S) {
+        __m256i X = _mm256_loadu_si256((__m256i*)(x+i));
+        // SIMD_condition
+        __m256i con = _mm256_and_si256(_mm256_cmpgt_epi32(RIGHT, X), _mm256_cmpgt_epi32(X, LEFT));
+        
+        // 除了 AVX-512 貌似没有办法转换为掩码，因此只好暂时出此下册，在内存中手动算
+        _mm256_storeu_si256((__m256i*)mask_buffer, con);
+        int mask = 0;
+        for (int i = 0; i < AVX_S; ++i) {
+                int flag = mask_buffer[i] != 0;
+                mask += flag << i;
+        }
+        if (mask != 0) {
+            for (int j = 0; j < AVX_S; ++j) {
+                if (mask >> j & 1){
+                    res.push_back(*(y+i+j));
+                }
+            }
+            
+        }
+    }
+
+    // 说明前面都没有 match，那么对剩下的部分计算
+    for (; i < size; ++i) {
+        if (left <= x[i] && x[i] <= right){
+            res.push_back(y[i]);
+        }
+    }
+    return res;
+}
 
 int get_matches_sum_simd(int *x, int *y, int size, int left, int right);
 
--- ex_3_0_1/test.cpp
+++ ex_3_0_1/test.cpp
@@ -2,12 +2,28 @@
 #include<iostream>
 using namespace std;
 
-
-int main() {
-    int x[] = {34, 65, 25, 26 , 7, 56, 78, 99, 1};
+void test_first_match() {
+    int x[] = {34, 65, 25, 26 , 27, 56, 78, 9, 1};
     int y[] = {34, 5, 25, 6 , 7, 56, 78, 99, 100};
     int size = 9;
     int left = 0;
     int right = 10;
     cout << find_first_match_simd(x, y, size, left, right);
+}
+
+void test_all_match() {
+    int x[] = {34, 65, 5, 26 , 27, 56, 78, 9, 1};
+    int y[] = {34, 5, 25, 6 , 7, 56, 78, 99, 100};
+    int size = 9;
+    int left = 0;
+    int right = 10;
+    vector<int> res = find_all_matches_simd(x, y, size, left, right);
+    for (int i = 0; i < res.size(); ++i) {
+        cout << res[i] << " ";
+    }
+}
+
+
+int main() {
+    test_all_match();
 }
