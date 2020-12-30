939b28f4de77fd3c7cd871ce4aff30dd3c6c42e7
dadagum <471944704@qq.com>
2020-12-30 12:37:07 +0000

    verify avx matrix multiplication

--- ex_3_0/avx_test.cpp
+++ ex_3_0/avx_test.cpp
@@ -4,6 +4,7 @@
 #include<cstring>
 #include<malloc.h>
 #include<omp.h>
+#include<vector>
 #include"rand_matrix.h"
 #include"matrix_mul_avx.h"
 using namespace std;
@@ -11,44 +12,64 @@ using namespace std;
 // avx 对齐要求
 const int BYTE_SIZE = 32; 
 
-// 矩阵大小: N * N
-static int matrixs[] = {512, 1024, 2048, 4096};
-const int m_size = 4;
+// 默认参数
+int N = 4096;
+int B = 64;
+float seed = 0.783943;
 
-const float seed = 0.783943;
-
-int main() {
-    for (int i = 0; i < m_size; ++i) {
-        int N = matrixs[i];
-        for (int B = 8; B <= N; B <<= 1) {
-            // avx 需要 256 位内存对齐
-            float *a = (float*) memalign(BYTE_SIZE, sizeof(float)*N*N);
-            float *b = (float*) memalign(BYTE_SIZE, sizeof(float)*N*N);
-            float *c = (float*) memalign(BYTE_SIZE, sizeof(float)*N*N);
-            memset(c, 0, sizeof(float)*N*N);
-            matrix_gen(a, b, N, seed);
-
-            // timer start
-            struct timeval start;
-            struct timeval end;
-            unsigned long diff;
-            gettimeofday(&start, NULL);
-            // exec multply
-            matrix_multply_avx(a, b, c, N, B);
-            // timer end
-            gettimeofday(&end, NULL);
-            diff =  1e6 * (end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec);
-    
-            // 打印结果
-            cout << "AVX: 矩阵 N = " << N << " ， 分块 B = " << B << " ，耗时 : " << diff << "ms" << endl;
-            // 释放内存
-            free(a);
-            free(b);
-            free(c);
+// 验证矩阵乘法
+float verify(float* c) {
+    vector<float> vt;
+    for (int i = 0; i < N; ++i) {
+        float cur_max = 0;
+        for (int j = 0; j < N; ++j) {
+            if (cur_max < c[i*N + j]) cur_max = c[i*N + j];
         }
-
+        vt.push_back(cur_max);
+        
     }
-    return 0;
+
+    float ans = vt[0];
+    for (int i = 1; i < N; ++i) {
+        if (ans > vt[i]) ans = vt[i];
+    }
+    return ans;
 }
 
+int main(int argc, char const *argv[])
+{
+    if (argc == 4) {
+        N = atoi(argv[1]);
+        B = atoi(argv[2]);
+        seed = atof(argv[3]);
+    }
+    
+    /* code */
+    // avx 需要 256 位内存对齐
+    float *a = (float*) memalign(BYTE_SIZE, sizeof(float)*N*N);
+    float *b = (float*) memalign(BYTE_SIZE, sizeof(float)*N*N);
+    float *c = (float*) memalign(BYTE_SIZE, sizeof(float)*N*N);
+    memset(c, 0, sizeof(float)*N*N);
+    matrix_gen(a, b, N, seed);
+
+    // timer start
+    struct timeval start;
+    struct timeval end;
+    unsigned long diff;
+    gettimeofday(&start, NULL);
+    // exec multply
+    matrix_multply_avx(a, b, c, N, B);
+    // timer end
+    gettimeofday(&end, NULL);
+    diff =  1e6 * (end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec);
+
+    // 打印结果
+    cout << "AVX: 矩阵 N = " << N << " ， 分块 B = " << B << " ，耗时 : " << diff << "us" << endl;
+    cout << "verify value = " << verify(c) << endl;
+    // 释放内存
+    free(a);
+    free(b);
+    free(c);
+    return 0;
+}
 
--- ex_3_0/matrix_mul_avx.cpp
+++ ex_3_0/matrix_mul_avx.cpp
@@ -18,17 +18,17 @@ void matrix_multply_avx(float *a, float *b, float *c, int N, int B) {
                         // 手动计算 8*8 矩阵
                         // load
                         __m256 vc0, vc1, vc2, vc3, vc4, vc5, vc6, vc7, vc8, vb;
-                        vc0 = _mm256_load_ps(&c[ii*N+jj]);
-                        vc1 = _mm256_load_ps(&c[(ii+1)*N+jj]);
-                        vc2 = _mm256_load_ps(&c[(ii+2)*N+jj]);
-                        vc3 = _mm256_load_ps(&c[(ii+3)*N+jj]);
-                        vc4 = _mm256_load_ps(&c[(ii+4)*N+jj]);
-                        vc5 = _mm256_load_ps(&c[(ii+5)*N+jj]);
-                        vc6 = _mm256_load_ps(&c[(ii+6)*N+jj]);
-                        vc7 = _mm256_load_ps(&c[(ii+7)*N+jj]);
+                        vc0 = _mm256_loadu_ps(&c[ii*N+jj]);
+                        vc1 = _mm256_loadu_ps(&c[(ii+1)*N+jj]);
+                        vc2 = _mm256_loadu_ps(&c[(ii+2)*N+jj]);
+                        vc3 = _mm256_loadu_ps(&c[(ii+3)*N+jj]);
+                        vc4 = _mm256_loadu_ps(&c[(ii+4)*N+jj]);
+                        vc5 = _mm256_loadu_ps(&c[(ii+5)*N+jj]);
+                        vc6 = _mm256_loadu_ps(&c[(ii+6)*N+jj]);
+                        vc7 = _mm256_loadu_ps(&c[(ii+7)*N+jj]);
 
                         for (int kk = k; kk + AVX_F_CAPACITY <= k+B; kk += AVX_F_CAPACITY) {
-                            vb = _mm256_load_ps(&b[kk*N + jj]);
+                            vb = _mm256_loadu_ps(&b[kk*N + jj]);
                             // gcc 需要加上 -mfma 选项
                             vc0 = _mm256_fmadd_ps(_mm256_broadcast_ss(&a[ii*N + kk]), vb, vc0);
                             vc1 = _mm256_fmadd_ps(_mm256_broadcast_ss(&a[(ii+1)*N + kk]), vb, vc1);
@@ -39,7 +39,7 @@ void matrix_multply_avx(float *a, float *b, float *c, int N, int B) {
                             vc6 = _mm256_fmadd_ps(_mm256_broadcast_ss(&a[(ii+6)*N + kk]), vb, vc6);
                             vc7 = _mm256_fmadd_ps(_mm256_broadcast_ss(&a[(ii+7)*N + kk]), vb, vc7);
 
-                            vb = _mm256_load_ps(&b[(kk+1)*N + jj]);
+                            vb = _mm256_loadu_ps(&b[(kk+1)*N + jj]);
                             vc0 = _mm256_fmadd_ps(_mm256_broadcast_ss(&a[ii*N + kk + 1]), vb, vc0);
                             vc1 = _mm256_fmadd_ps(_mm256_broadcast_ss(&a[(ii+1)*N + kk + 1]), vb, vc1);
                             vc2 = _mm256_fmadd_ps(_mm256_broadcast_ss(&a[(ii+2)*N + kk + 1]), vb, vc2);
@@ -49,7 +49,7 @@ void matrix_multply_avx(float *a, float *b, float *c, int N, int B) {
                             vc6 = _mm256_fmadd_ps(_mm256_broadcast_ss(&a[(ii+6)*N + kk + 1]), vb, vc6);
                             vc7 = _mm256_fmadd_ps(_mm256_broadcast_ss(&a[(ii+7)*N + kk + 1]), vb, vc7);
 
-                            vb = _mm256_load_ps(&b[(kk+2)*N + jj]);
+                            vb = _mm256_loadu_ps(&b[(kk+2)*N + jj]);
                             vc0 = _mm256_fmadd_ps(_mm256_broadcast_ss(&a[ii*N + kk + 2]), vb, vc0);
                             vc1 = _mm256_fmadd_ps(_mm256_broadcast_ss(&a[(ii+1)*N + kk + 2]), vb, vc1);
                             vc2 = _mm256_fmadd_ps(_mm256_broadcast_ss(&a[(ii+2)*N + kk + 2]), vb, vc2);
@@ -60,7 +60,7 @@ void matrix_multply_avx(float *a, float *b, float *c, int N, int B) {
                             vc7 = _mm256_fmadd_ps(_mm256_broadcast_ss(&a[(ii+7)*N + kk + 2]), vb, vc7);
 
  
-                            vb = _mm256_load_ps(&b[(kk+3)*N + jj]);
+                            vb = _mm256_loadu_ps(&b[(kk+3)*N + jj]);
                             vc0 = _mm256_fmadd_ps(_mm256_broadcast_ss(&a[ii*N + kk + 3]), vb, vc0);
                             vc1 = _mm256_fmadd_ps(_mm256_broadcast_ss(&a[(ii+1)*N + kk + 3]), vb, vc1);
                             vc2 = _mm256_fmadd_ps(_mm256_broadcast_ss(&a[(ii+2)*N + kk + 3]), vb, vc2);
@@ -70,16 +70,56 @@ void matrix_multply_avx(float *a, float *b, float *c, int N, int B) {
                             vc6 = _mm256_fmadd_ps(_mm256_broadcast_ss(&a[(ii+6)*N + kk + 3]), vb, vc6);
                             vc7 = _mm256_fmadd_ps(_mm256_broadcast_ss(&a[(ii+7)*N + kk + 3]), vb, vc7);
 
+                            vb = _mm256_loadu_ps(&b[(kk+4)*N + jj]);
+                            vc0 = _mm256_fmadd_ps(_mm256_broadcast_ss(&a[ii*N + kk + 4]), vb, vc0);
+                            vc1 = _mm256_fmadd_ps(_mm256_broadcast_ss(&a[(ii+1)*N + kk + 4]), vb, vc1);
+                            vc2 = _mm256_fmadd_ps(_mm256_broadcast_ss(&a[(ii+2)*N + kk + 4]), vb, vc2);
+                            vc3 = _mm256_fmadd_ps(_mm256_broadcast_ss(&a[(ii+3)*N + kk + 4]), vb, vc3);
+                            vc4 = _mm256_fmadd_ps(_mm256_broadcast_ss(&a[(ii+4)*N + kk + 4]), vb, vc4);
+                            vc5 = _mm256_fmadd_ps(_mm256_broadcast_ss(&a[(ii+5)*N + kk + 4]), vb, vc5);
+                            vc6 = _mm256_fmadd_ps(_mm256_broadcast_ss(&a[(ii+6)*N + kk + 4]), vb, vc6);
+                            vc7 = _mm256_fmadd_ps(_mm256_broadcast_ss(&a[(ii+7)*N + kk + 4]), vb, vc7);
+
+                            vb = _mm256_loadu_ps(&b[(kk+5)*N + jj]);
+                            vc0 = _mm256_fmadd_ps(_mm256_broadcast_ss(&a[ii*N + kk + 5]), vb, vc0);
+                            vc1 = _mm256_fmadd_ps(_mm256_broadcast_ss(&a[(ii+1)*N + kk + 5]), vb, vc1);
+                            vc2 = _mm256_fmadd_ps(_mm256_broadcast_ss(&a[(ii+2)*N + kk + 5]), vb, vc2);
+                            vc3 = _mm256_fmadd_ps(_mm256_broadcast_ss(&a[(ii+3)*N + kk + 5]), vb, vc3);
+                            vc4 = _mm256_fmadd_ps(_mm256_broadcast_ss(&a[(ii+4)*N + kk + 5]), vb, vc4);
+                            vc5 = _mm256_fmadd_ps(_mm256_broadcast_ss(&a[(ii+5)*N + kk + 5]), vb, vc5);
+                            vc6 = _mm256_fmadd_ps(_mm256_broadcast_ss(&a[(ii+6)*N + kk + 5]), vb, vc6);
+                            vc7 = _mm256_fmadd_ps(_mm256_broadcast_ss(&a[(ii+7)*N + kk + 5]), vb, vc7);
+
+                            vb = _mm256_loadu_ps(&b[(kk+6)*N + jj]);
+                            vc0 = _mm256_fmadd_ps(_mm256_broadcast_ss(&a[ii*N + kk + 6]), vb, vc0);
+                            vc1 = _mm256_fmadd_ps(_mm256_broadcast_ss(&a[(ii+1)*N + kk + 6]), vb, vc1);
+                            vc2 = _mm256_fmadd_ps(_mm256_broadcast_ss(&a[(ii+2)*N + kk + 6]), vb, vc2);
+                            vc3 = _mm256_fmadd_ps(_mm256_broadcast_ss(&a[(ii+3)*N + kk + 6]), vb, vc3);
+                            vc4 = _mm256_fmadd_ps(_mm256_broadcast_ss(&a[(ii+4)*N + kk + 6]), vb, vc4);
+                            vc5 = _mm256_fmadd_ps(_mm256_broadcast_ss(&a[(ii+5)*N + kk + 6]), vb, vc5);
+                            vc6 = _mm256_fmadd_ps(_mm256_broadcast_ss(&a[(ii+6)*N + kk + 6]), vb, vc6);
+                            vc7 = _mm256_fmadd_ps(_mm256_broadcast_ss(&a[(ii+7)*N + kk + 6]), vb, vc7);
+
+                            vb = _mm256_loadu_ps(&b[(kk+7)*N + jj]);
+                            vc0 = _mm256_fmadd_ps(_mm256_broadcast_ss(&a[ii*N + kk + 7]), vb, vc0);
+                            vc1 = _mm256_fmadd_ps(_mm256_broadcast_ss(&a[(ii+1)*N + kk + 7]), vb, vc1);
+                            vc2 = _mm256_fmadd_ps(_mm256_broadcast_ss(&a[(ii+2)*N + kk + 7]), vb, vc2);
+                            vc3 = _mm256_fmadd_ps(_mm256_broadcast_ss(&a[(ii+3)*N + kk + 7]), vb, vc3);
+                            vc4 = _mm256_fmadd_ps(_mm256_broadcast_ss(&a[(ii+4)*N + kk + 7]), vb, vc4);
+                            vc5 = _mm256_fmadd_ps(_mm256_broadcast_ss(&a[(ii+5)*N + kk + 7]), vb, vc5);
+                            vc6 = _mm256_fmadd_ps(_mm256_broadcast_ss(&a[(ii+6)*N + kk + 7]), vb, vc6);
+                            vc7 = _mm256_fmadd_ps(_mm256_broadcast_ss(&a[(ii+7)*N + kk + 7]), vb, vc7);
+
                         }
                         // store
-                        _mm256_store_ps(&c[ii*N+jj], vc0);
-                        _mm256_store_ps(&c[(ii+1)*N+jj], vc1);
-                        _mm256_store_ps(&c[(ii+2)*N+jj], vc2);
-                        _mm256_store_ps(&c[(ii+3)*N+jj], vc3);
-                        _mm256_store_ps(&c[(ii+4)*N+jj], vc4);
-                        _mm256_store_ps(&c[(ii+5)*N+jj], vc5);
-                        _mm256_store_ps(&c[(ii+6)*N+jj], vc6);
-                        _mm256_store_ps(&c[(ii+7)*N+jj], vc7);
+                        _mm256_storeu_ps(&c[ii*N+jj], vc0);
+                        _mm256_storeu_ps(&c[(ii+1)*N+jj], vc1);
+                        _mm256_storeu_ps(&c[(ii+2)*N+jj], vc2);
+                        _mm256_storeu_ps(&c[(ii+3)*N+jj], vc3);
+                        _mm256_storeu_ps(&c[(ii+4)*N+jj], vc4);
+                        _mm256_storeu_ps(&c[(ii+5)*N+jj], vc5);
+                        _mm256_storeu_ps(&c[(ii+6)*N+jj], vc6);
+                        _mm256_storeu_ps(&c[(ii+7)*N+jj], vc7);
                     }
                 }
 
